2f1b183afcad88bb54f3d6928d5bea7e7e697120
Renney Namale <renney003@gmail.com>
2024-04-10 21:54:19 +0300

    Include the users and images api endpoints

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('upload', views.upload, name='upload'),
-    path('detailsave', views.detail_save, name='detail_save')
+    path('', views.index, name='index',),
+    path('upload', views.upload, name='upload',),
+    path('detailsave', views.detail_save, name='detail_save',),
+    path("image-view", views.ImageListView.as_view(), name="image-view",),
+    path("image-detail/<int:pk>", views.ImageDetailView.as_view(), name="image-detail",),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- home/views.py
+++ home/views.py
@@ -2,9 +2,29 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import RequestContext
 from django.contrib import messages
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
+from rest_framework import permissions, viewsets
+from django.http import JsonResponse, Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.request import Request
+from rest_framework.generics import GenericAPIView
 
 from .models import Image
+from .serializers import GroupSerializer, UserSerializer, ImageSerializer
+
+import os
+import time
+import json
+from pathlib import Path
+
+from urllib.request import urlretrieve
+
+# using requests library
+import requests
+
+API_KEY = os.environ.get('API_KEY')
 
 # Create your views here.
 
@@ -36,6 +56,91 @@ def detail_save(request):
         messages.success(request, "Image Uploaded Successfully!")
         return redirect('upload')
     else:
-        messages.warning(request, 'Signup first!')
+        messages.warning(request, 'Signup First!')
         return redirect('upload')
+
+
+def image_ai_enhance(request):
+    pass
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer(queryset)
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all().order_by('name')
+    serializer_class = GroupSerializer(queryset)
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class ImageViewSet(viewsets.ModelViewSet):
+    
+    queryset = Image.objects.all().order_by('name')
+    serializer_class = ImageSerializer(queryset)
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class ImageListView(GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request, format=None):
+        queryset = Image.objects.all().order_by('name')
+        serializer= ImageSerializer(queryset, many=True)
+        # return Response(serializer.data)
+
+
+        serializer_context = {
+            'request': request,
+        }
+
+        import pdb; pdb.set_trace()
+
+        return Response(ImageSerializer(serializer, context=serializer_context).data, status=status.HTTP_200_OK)
+
+    def post(self, request, format=None):
+        serializer = ImageSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ImageDetailView(APIView):
+    """
+    Retrieve, update or delete an image instance.
+    """
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self, pk):
+        try:
+            return Image.objects.get(pk=pk)
+        except Image.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        image = self.get_object(pk)
+        serializer = ImageSerializer(image)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        image = self.get_object(pk)
+        serializer = ImageSerializer(image, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        image = self.get_object(pk)
+        image.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
