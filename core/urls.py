d0a3ae827c3d52da683cd20e46de9a96c482b8a2
Renney Namale <renney003@gmail.com>
2024-04-10 21:53:02 +0300

    Include the url for DRF

--- core/urls.py
+++ core/urls.py
@@ -18,10 +18,33 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.contrib.auth.models import User
+from rest_framework import routers, serializers, viewsets
+
+from home import views
+
+# Serializers define the API representation.
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'is_staff']
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
 urlpatterns = [
     path('', include('home.urls')),
     path("admin/", admin.site.urls),
-    path("", include('admin_argon.urls'))
+    path("admin_argon/", include('admin_argon.urls')),
+    path('api/', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
 
 
