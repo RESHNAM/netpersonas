ac499847b82968cea098f5411fbce800ad20d90c
Renney Namale <renney003@gmail.com>
2024-04-11 23:22:50 +0300

    url for user detail endpoint

--- core/urls.py
+++ core/urls.py
@@ -45,6 +45,7 @@ urlpatterns = [
     path("admin_argon/", include('admin_argon.urls')),
     path('api/', include(router.urls)),
     path('api/new-user', views.NewUserList.as_view(), name='new-user'),
+    path('api/users/<int:pk>', views.UserDetailView.as_view(), name='user-detail'),
     path('api/image-view', views.ImageListView.as_view(), name='image-view'),
     path('api/image-detail/<int:pk>', views.ImageDetailView.as_view(), name='image-detail'),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
