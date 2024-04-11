2ff2b85c839a0a6003b2a0bfbea6aecc322ba999
Renney Namale <renney003@gmail.com>
2024-04-11 23:09:07 +0300

    update the home urls

--- home/urls.py
+++ home/urls.py
@@ -6,9 +6,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index',),
     path('upload', views.upload, name='upload',),
-    path('detailsave', views.detail_save, name='detail_save',),
-    path("image-view", views.ImageListView.as_view(), name="image-view",),
-    path("image-detail/<int:pk>", views.ImageDetailView.as_view(), name="image-detail",),
+    path('detailsave', views.detail_save, name='detail_save',)
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
