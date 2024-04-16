12a40d4f4f2f4776c832c00e131275cabe23d8d0
Renney Namale <renney003@gmail.com>
2024-04-16 08:59:40 +0300

    Feedbacl model plus migrations

--- home/models.py
+++ home/models.py
@@ -15,3 +15,16 @@ class Image(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Feedback(models.Model):
+    name = models.CharField(max_length=200, help_text="Name of the sender")
+    email = models.EmailField(max_length=200)
+    subject = models.CharField(max_length=200)
+    message = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = "Feedback"
+
+    def __str__(self):
+        return self.name + "-" +  self.email
