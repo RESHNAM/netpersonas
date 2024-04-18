79cd0a01516d06dcddb8c2088230714be51ba9bb
Renney Namale <renney003@gmail.com>
2024-04-18 05:31:40 +0300

    include feedback to admin dashboard

--- home/admin.py
+++ home/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 
-from .models import Image
+from .models import Image, Feedback
 
 # Register your models here.
 @admin.register(Image)
 class ImageAdmin(admin.ModelAdmin):
     list_display = ("author","name","cover","created_at","updated_at")
+
+@admin.register(Feedback)
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display = ("name","email","subject","message","date")
