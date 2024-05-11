4d3a94bf53a451ad15f42f3727e6757099e2d145
Renney Namale <renney003@gmail.com>
2024-05-11 16:18:29 +0300

    fix the API Views on the dashboard backend

--- home/urls.py
+++ home/urls.py
@@ -7,13 +7,13 @@ urlpatterns = [
     path('', views.index, name='index',),
     path('upload', views.upload, name='upload',),
     path('detailsave', views.detail_save, name='detail_save',),
-    path('process-image/<int:pk>', views.Denoidebluenha, name='process-image',),
+    path('process-image/<int:pk>/', views.Denoidebluenha, name='process-image',),
     path('contactus', views.feedback, name='feedback',),
     # path('api/', include(router.urls)),
     path('api/new-user', views.NewUserList.as_view(), name='new-user'),
-    path('api/users/<int:pk>', views.UserDetailView.as_view(), name='user-detail'),
+    path('api/users/<int:pk>/', views.UserDetailView.as_view(), name='user-detail'),
     path('api/image-view', views.ImageListView.as_view(), name='image-view'),
-    path('api/image-detail/<int:pk>', views.ImageDetailView.as_view(), name='image-detail'),
+    path('api/image-detail/<int:pk>/', views.ImageDetailView.as_view(), name='image-detail'),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework_api'))
 ]
 
