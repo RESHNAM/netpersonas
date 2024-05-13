48192c448bd7b1e1f40021e914c139c111656fe3
Renney Namale <renney003@gmail.com>
2024-05-13 14:12:30 +0300

    include get_all_users module

--- home/views.py
+++ home/views.py
@@ -11,6 +11,8 @@ from rest_framework import status
 from rest_framework.request import Request
 from rest_framework.generics import GenericAPIView
 from rest_framework import generics
+from django.contrib.sessions.models import Session
+from django.utils import timezone
 
 from .models import Image, Feedback
 from django.contrib import messages
@@ -277,3 +279,18 @@ def feedback(request):
         feedb = FeedbackForm()
     return render(request, 'pages/feedback.html', {'form': feedb})
 
+
+def get_all_logged_in_users():
+    # Query all non-expired sessions
+    # use timezone.now() instead of datetime.now() in latest versions of Django
+    sessions = Session.objects.filter(expire_date__gte=timezone.now())
+    uid_list = []
+
+    # Build a list of user ids from that query
+    for session in sessions:
+        data = session.get_decoded()
+    uid_list.append(data.get('_auth_user_id', None))
+
+    # Query all logged in users based on id list
+    print("CONFIRM: ",User.objects.filter(id__in=uid_list))
+    return User.objects.filter(id__in=uid_list)
