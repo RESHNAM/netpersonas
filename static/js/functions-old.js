5108cfb42db6b35795ff4711872b6b636f747c9d
Renney Namale <renney003@gmail.com>
2024-05-17 16:19:18 +0300

    include the font awesome .woff file

--- static/js/functions-old.js
+++ static/js/functions-old.js
@@ -171,7 +171,7 @@
 
 }(jQuery));
 
-// <!-- Google Analytics -->
+<!-- Google Analytics -->
 
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -181,4 +181,4 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 ga('create', 'UA-89540131-1', 'auto');
 ga('send', 'pageview');
 
-// <!-- End Google Analytics -->
+<!-- End Google Analytics -->
